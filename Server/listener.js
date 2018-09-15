3734ee4e5210529f5288e46174fa7215d81f496e
Georgia Shay <georgiaeshay@Georgias-MacBook-Pro.local>
2018-09-15 18:01:45 -0400

    Fixed camel case

--- Server/listener.js
+++ Server/listener.js
@@ -1,66 +1,114 @@
 const http = require("http");
 const port = 3000;
 
+//const API_TOKEN = "39248212264-ha3tf2b00lhvf799oqks0mp73q4mv5ga.apps.googleusercontent.com"
+const API_TOKEN = "AIzaSyDXvFg03qsCUN6vW4XpMk0vD6oCnwdWFsk";
+
 //const FileReader = require("filereader");
 var FileAPI = require("file-api"), File = FileAPI.File, FileReader = FileAPI.FileReader;
 const express = require("express");
 const bodyParser = require("body-parser");
 const fs = require("fs");
-
+const request = require("request");
+const vision = require("@google-cloud/vision");
+//if this is put on a different computer, you must set options (they are in local variables on mine)
+const client = new vision.ImageAnnotatorClient();
 const app = express();
-const API_TOKEN = "1060304981152-si250lqq588r02461anohh8uj0g6207v.apps.googleusercontent.com"
 
-//app.post("/")
+app.use(bodyParser.json());
 
-function GetAPIResponse(img) {
-  var post_data = JSON.stringify({
-    "requests":  [
-      {
-        "image": {
-          "content": img
-        },
-        "features": [
-          {
-            "type": "TEXT_DETECTION"
-          }
-        ]
-      }
-    ]
-  });
+const average = arr => arr.reduce( ( p, c ) => p + c, 0 ) / arr.length;
 
-  var post_options = {
-    host: "https://vision.googleapis.com/v1/images:annotate",
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "Authorization": "Bearer " + API_TOKEN
+async function parseVision(img) {
+  var request = {
+    "image": {
+      "content": img
     }
-  }
+    // "features": [
+    //   {
+    //     "type": "TEXT_DETECTION"
+    //   }
+    // ]
+  };
 
-  var post_req = http.request(post_options, function(res) {
-    console.log(res);
-  });
-
-  post_req.write(post_data);
-  post_req.end();
+  var results = await client.documentTextDetection(request);
+  return results;
 }
 
 function encodeImage(filePath) {
-  // var file = new File(filePath);
-  // console.log(file);
-  // var reader = new FileReader();
-  // console.log("got reader");
-  // //reader.setNodeChunkEncoding(false);
-  // reader.onloadend = function() {
-  //   console.log("RESULT", reader.result);
-  //   reader.readAsDataURL(file);
-  // }
   var bitmap = fs.readFileSync(filePath);
   var encoded = new Buffer(bitmap).toString("base64");
   return encoded;
 }
 
-var encodedImage = encodeImage("/Users/georgiaeshay/Downloads/1_3wn7FLjhZ3aynRQyTdDD4Q.jpeg");
-//GetAPIResponse(encodedImage);
+async function getVisionResults(img) {
+  var visionResults = await parseVision(encodedImage);
+  console.log("got vision results");
+  jsonString = JSON.stringify(visionResults);
+  fs.writeFile("results.json", jsonString, "utf8");
+}
+
+function fixCamelCase(visionResults) {
+  var words = [[visionResults[0].textAnnotations[1].description]];
+  var wordsIndex = 0;
+  var xDiffs = [[]];
+  for (var i = 1; i < visionResults[0].textAnnotations.length-1; i++) {
+    var thisAnnotation = visionResults[0].textAnnotations[i];
+    var nextAnnotation = visionResults[0].textAnnotations[i+1];
+    xDifference = nextAnnotation.boundingPoly.vertices[0].x - thisAnnotation.boundingPoly.vertices[1].x;
+    yDifference = nextAnnotation.boundingPoly.vertices[0].y - thisAnnotation.boundingPoly.vertices[0].y;
+    if (xDifference < 0 && yDifference > 0) {
+      wordsIndex++;
+      words.push([]);
+      xDiffs.push([]);
+    } else {
+      xDiffs[wordsIndex].push(xDifference);
+    }
+    words[wordsIndex].push(nextAnnotation.description);
+  }
+
+  var wordStrings = [];
+  for (var i = 0; i < words.length; i++) {
+    if (xDiffs[i].length > 1) {
+      averageXDiff = average(xDiffs[i]);
+      var wordString = words[i][0];
+      for (var j = 0; j < xDiffs[i].length; j++) {
+        if (xDiffs[i][j] < averageXDiff) {
+          wordString += words[i][j+1];
+        } else {
+          wordString += " " + words[i][j+1];
+        }
+      }
+      wordStrings.push(wordString);
+    } else {
+      wordStrings.push(words[i].join(" "));
+    }
+  }
+
+  return wordStrings.join("\n");
+}
+
+
+fs.readFile("results.json", "utf8", function readFileCallback(err, data) {
+  if(err) {
+    console.log(err);
+  } else {
+    var visionResults = JSON.parse(data);
+    console.log(fixCamelCase(visionResults));
+
+  }
+});
+
+
+app.post("/image", async function(req, res) {
+  var encodedImage = req.body.img;
+  var targetAddress = req.body.ip;
+
+  var visionAPIResults = getVisionResults(encodedImage);
+  var codeString = fixCamelCase(visionAPIResults);
+
+  sendCode(codeString, targetAddress);
+});
+
 
 app.listen(8080);
